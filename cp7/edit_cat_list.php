27be37c346d001e6e905208899ed46a66b9a9ef2
abdelhakim-nekkache <hakimnekkache@gmail.com>
2021-02-04 16:57:32 +0100

    new

---
+++ cp7/edit_cat_list.php
@@ -0,0 +1,100 @@
+<?php
+// Récupére la page active si existe
+if (isset($_GET['pg']) && !empty($_GET['pg'])){
+    $pg = (int) $_GET['pg'];
+} else {
+    $pg = 1;
+}
+// Récupére le nombre de ligne active si existe
+if (isset($_GET['nb']) && !empty($_GET['nb'])){
+    $nb = (int) $_GET['nb'];
+} else {
+    $nb = 5;
+}
+// Ouvre la BDD en MYSQLI
+$start = ($pg - 1) * $nb;
+$cnn = mysqli_connect('localhost', 'root', 'greta', 'northwind');
+$res = mysqli_query($cnn, 'SELECT * FROM categories LIMIT {$start}, {$nb}');
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Northwind Traders</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+</head>
+
+<body class="container">
+    <h1>Liste des catégories</h1>
+
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="index.php">Accueil</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Liste catégories</li>
+        </ol>
+    </nav>
+
+<a href="edit_cat_form.php" class="btn btn-success">Ajouter une catégorie</a>
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <?php
+                // Liste les colonnes
+                $html = '';
+                while ($col = mysqli_fetch_field($res)) {
+                    $html .= "<th>{$col->name}</th>";
+                }
+                echo $html;
+                ?>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            // Liste les data
+            $html = "";
+            while ($row = mysqli_fetch_row($res)) {
+                $html .= "<tr>";
+                foreach ($row as $key => $val) {
+                    // Lien si 1ère colonne
+                    if ($key === 0) {
+                        $html .= '<td><a href="edit_cat_form.php?k=' . $val . '">' . $val . '</a></td>';
+                    }
+                    // Si c'est  du BLOB
+                    elseif (strpos($val, ';base64,')) {
+                        $html .= '<td><img src="' . $val . '" style="width:5rem"/></td>';
+                    } else {
+                        $html .= "<td>{$val}</td>";
+                    }
+                }
+                $html .= "</tr>";
+            }
+            echo $html;
+            ?>
+        </tbody>
+    </table>
+
+    <nav>
+        <ul class="pagination">
+            <?php
+            // Calcule la pagination
+            $res = mysqli_query ($cnn, "SELECT COUNT(*) AS total FROM categories");
+            $row = mysqli_fetch_assoc($res);
+            $pgs = ceil($row['total'] / $nb);
+
+            // Affiche la page
+            $html = '';
+            for ($i = i ; $i <= $pgs ; $i++){
+                $href=$_SERVER['PHP_SELF'].'?pg='.$i.'& nb='.$nb;
+            }
+            ?>
+
+        </ul>
+    </nav>
+
+</body>
+
+</html>
--- cp7/index.php
+++ cp7/index.php
@@ -4,90 +4,115 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Northwind Traders | Accueil</title>
+    <title>Northwind Traders</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/main.css">
 </head>
 
 <body class="container">
     <div class="jumbotron">
         <h1 class="display-4">Northwind Traders</h1>
-
-        <?php
-
-        include_once('team.php');
-        define("FNAME", $members[11][0]);
-        $diff = (strtotime(date('Y-m-d')) - strtotime('2020/11/02')) / 60 / 60 / 24;
-        echo '<p class="lead">Projet réalisé par ' . FNAME . ', Daron Codeur depuis ' . $diff . ' jours.</p>';
-
-        ?>
-
+        <p class="lead">Projet fil rouge en HTML, CSS, JS, PHP et MySQL basé sur le jeu de données Northwind.
+            <?php
+            include_once('team.php');
+            $diff = (strtotime(date('Y-m-d')) - strtotime('2020-11-02')) / 60 / 60 / 24;
+            echo ' Développé par ' . PRENOM . ', Daron Coder depuis ' . $diff . ' jours.';
+            ?>
+        </p>
         <hr class="my-4">
-        <p>Cliquer sur le bouton ci-dessous pour accéder au back-office :</p>
-        <a class="btn btn-success btn-lg" href="#" role="button">Connexion</a>
+        <p>Cliquer sur le bouton ci-dessous pour accéder au back-office (user et mot de passe requis) :</p>
+        <a class="btn btn-success btn-lg" href="login.php" role="button">Connexion</a>
+        <a class="btn btn-primary" href="#" data-toggle="modal" data-target="#staticBackdrop">Inscription</a>
     </div>
 
-    <h2>Notre équipe</h2>
-
-    <section id="team" class="d-flex flex-wrap justify-content-between">
-
+    <h2>Membres de l'équipe</h2>
+    <section id="team" class="d-flex flex-wrap justify-content-around">
         <?php
-        // include_once('team.php');
-        // la ligne ci-dessus n'est plus utile car on l'utilise finalement plus haut dans le code (ligne 18)
-        $html = "";
+        $html = '';
         for ($i = 0; $i < count($members); $i++) {
-            $html .= '<div class="card mb-3 ' . ($members[$i][2] === "F" ? "girl" : "boy") . '" style="width: 18rem;">';
+            $html .= '<div class="card mb-3 ' . ($members[$i][2] === 'F' ? 'girl' : 'boy') . '" style="width: 18rem;">';
             $html .= '<div class="card-body">';
             $html .= '<h5 class="card-title">' . $members[$i][0] . '</h5>';
             $html .= '<p class="card-text">' . $members[$i][1] . ' ans</p>';
-            // $html .= '<p class="card-text"> marié(e) : '. $members[$i][3] .'</p>';
-            // la ligne précédente renvoie "1" pour les personnes mariées 
-            // ('true' dans le tableau '$members' du fichier 'team.php')
-            // mais reste vide quand 'false' ; 
-            // je décide donc de rajouter la fonction 'str_replace()' (qui prend 3 paramètres : ce qu'on cherche, par quoi on remplace, et où on cherche)
-            // $html .= '<p class="card-text"> marié(e) : '. str_replace(true, "oui", $members[$i][3]) .'</p>';
-            // cela fonctionne bien pour le 'true' mais pas pour le 'false' car 'true' renvoie '1' et false '' 
-            // je décide finalement de faire un if else : 
-
-            /*
-            if ($members[$i][3]) {
-                $html .= '<p class="card-text"> marié(e) : oui</p>';
-            } else {
-                $html .= '<p class="card-text"> Célibataire</p>';
-            };
-            */
-            /*
-            // variante de mon if en mode 'ternaire' pour gérer "marié" ou "mariéE" :
-            if($members[$i][3]) {
-                $html .= ($members[$i][2] === "F" ? '<p class="card-text"> Mariée</p>' : '<p class="card-text"> Marié</p>' );
-            } else {
-                $html .= '<p class="card-text"> Célibataire</p>';
-            };
-            */
-            // variante de mon if en mode 'ternaire' dans un 'ternaire' pour éliminer le 'if' :
-            $html .= '<p class="card-text"> ' . ($members[$i][3] ? ($members[$i][2] === "F" ? "Mariée" : "Marié") : "Célibataire") . '</p>';
-
+            $html .= '<p class="card-text">' . ($members[$i][3] ? ($members[$i][2] === 'F' ? 'Mariée' : 'Marié') : 'Célibataire') . '</p>';
             $html .= '</div></div>';
         }
         echo $html;
-
         ?>
-
     </section>
 
     <h2>Nos références</h2>
-
     <section id="projects">
-
-        <?php
-            include_once('projects.php');
-        ?>
-
+        <?php include_once('projects.php'); ?>
     </section>
-    
-    <?php
-    include_once('functions.php');
-    ?>
+
+    <div id="bo">
+        <h2>Back-Office</h2>
+        <section id="tables">
+            <?php
+            $cnn = mysqli_connect('localhost', 'root', 'root', 'information_schema');
+            $res = mysqli_query($cnn, "select table_name, table_rows from tables where table_schema = 'northwind'");
+            $html = '';
+            while ($row = mysqli_fetch_assoc($res)) {
+                $html .= '<a class="btn btn-info m-3" href="' . $row['TABLE_NAME'] . '.php">' . $row['TABLE_NAME'] . ' <span class="badge badge-light">' . $row['TABLE_ROWS'] . '</span></a>';
+            }
+            echo $html;
+            mysqli_close($cnn);
+            ?>
+        </section>
+    </div>
+
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+    <form action="register.php" method="post">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Inscription</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+                <div>
+                    <label for="fname">Prénom :</label>
+                    <input type="text" name="fname" id="fname" pattern="" required class="form-control" pattern="[A-Za-z\U00FF\-' ]">
+
+                    <label for="mail">Courriel :</label>
+                    <input type="email" name="mail" id="mail" pattern="" required class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="pass">Mot de passe</label>
+                    <input type="password" name="pass" id="pass" pattern="[A-Za-z0-9@$*!? ]{8,}" required class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="check">Vérification du Mot de passe</label>
+                    <input type="password" id="check" pattern="[A-Za-z0-9@$*!? ]{8,}" required class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="land">Pays :</label>
+                    <select name="land" id="land" class="form-control">
+                        <?php
+                        $json=file_get_contents('https://restcountries.eu/rest/v2/lang/fr?fields=name;alpha2Code');
+                        $obj=json_decode($json);
+                        ?>
+                    </select>
+                </div>
+            
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+        <input type="submit" value="Valider" class="btn btn-primary">
+      </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 
 </body>
 
